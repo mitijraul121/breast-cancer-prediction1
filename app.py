30018aa7dca7a60b704260103a3d88d6b2dd3a2c
mitijraul121 <89200172+mitijraul121@users.noreply.github.com>
2024-04-02 09:54:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ def upload_file():
                                class_name=prediction)
     return render_template('index.html')
 
-
+'''
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))'''
